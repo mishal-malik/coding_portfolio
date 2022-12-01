bffa40c4c2d6df83478453fde45269cfb6f42cd6
mishal-malik <119406823+mishal-malik@users.noreply.github.com>
2022-12-01 17:10:39 +0500

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//Modify the file q3.cpp so that it finds and displays all the elements that have at least two greater elements in an array of integers.
 int main()
 {
     int arr[] = {95, 75, 86, 93, 76, 17, 23, 69, 40, 88, 67, 10, 37, 68, 83, 47, 11, 6, 61, 62, 0, 32, 29, 38, 36};
