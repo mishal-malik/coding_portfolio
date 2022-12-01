0d294b123bd11256d4b632a0e588f1c48e37eecf
mishal-malik <119406823+mishal-malik@users.noreply.github.com>
2022-12-01 17:08:06 +0500

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {95, 75, 86, 93, 76, 17, 23, 69, 40, 88, 67, 10, 37, 68, 83, 47, 11, 6, 61, 62, 0, 32, 29, 38, 36};
+    int l1, l2, l3;
+    // Write your code here
+    for( int i=0; i<3; i=i+1)
+    {
+      auto value= arr[i];
+
+      for( int j= i+1; j<25; j= j+1 )
+      {
+         if(arr[j] != value)
+        {
+          if(arr[j] >value)
+          {
+            value =arr[j];
+            arr[j]=arr[i];
+            arr[i]= value;
+
+          }
+        }
+        }
+    l1= arr[0];
+    l2= arr[1];
+    l3= arr[2];    
+}
+  
+
+    cout << l1 << " " << l2 << " " << l3 << endl;
+}
