5925adb59cf54d51c85d3fa790c45e329b4a343e
mishal-malik <119406823+mishal-malik@users.noreply.github.com>
2022-12-01 14:20:03 +0500

    Loop

--- problem5.cpp
+++ problem5.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+
+/*Write a C++ program that declares and initializes an array of the integer values 13, 4, 2,
+98, 42, 41, 29, 71, 15, 23. Use a for loop to iterate over the values in this array and calculate the
+average of the odd values in the array*/
+
+
 using namespace std;
 int main() {
 
