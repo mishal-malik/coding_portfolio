df17b271116ecba1600818b8885392ca867aac8f
mishal-malik <119406823+mishal-malik@users.noreply.github.com>
2022-12-01 14:16:43 +0500

    Batman

--- problem1.cpp
+++ problem1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+/*1: Take any pattern of ASCII art from https://asciiart.website/ and put the text into
+std::cout statements in your C++ program so that when you run the program, the corresponding
+ASCII art is displayed on the screen.*/
 
 int main() {
 cout << "_____________________\t\t\t\t\t\t\t   _____________________ "<<endl;
